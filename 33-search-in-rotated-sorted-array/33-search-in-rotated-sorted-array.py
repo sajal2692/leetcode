a44d17b48d583333c3cdd981a35ff1aafbc9687c
Sajal Sharma <sajal2692@gmail.com>
2022-09-09 16:08:34 +0530

    Time: 88 ms (10.79%), Space: 14.3 MB (55.62%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -4,24 +4,29 @@
 # [0,1,2] is the right sorted array
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # define left and right pointers
+        # define left and right pointers:
         left, right = 0, len(nums) - 1
         
-        while left <= right: 
-            mid = (left+right)//2
-            if nums[mid] == target:
+        while left <= right:
+            mid = (left + right) // 2
+            
+            # target found in the middle
+            if target == nums[mid]:
                 return mid
+            
             # left sorted array
             if nums[left] <= nums[mid]:
+                # within left sorted array, target > mid element or smaller than left edge
                 if target > nums[mid] or target < nums[left]:
                     left = mid + 1
                 else:
                     right = mid - 1
             # right sorted array
             else:
+                # within right sorted array, target < mid element or greater than right edge
                 if target < nums[mid] or target > nums[right]:
                     right = mid - 1
                 else:
                     left = mid + 1
-        # binary search complete and element not found
+        # target not found
         return -1
