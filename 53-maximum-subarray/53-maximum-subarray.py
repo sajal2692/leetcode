8a55620759f2764a51c056083957034362178e0d
Sajal Sharma <sajal2692@gmail.com>
2022-09-14 13:05:48 +0530

    Time: 1994 ms (5.01%), Space: 27.8 MB (96.08%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,16 +1,23 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         
+        # check edge case of empty array
         if len(nums) < 0:
             return 0
         
-        current_sum = 0
+        # initialize
+        running_sum = 0
         max_sum = nums[0]
         
+        # loop over array of nums
         for num in nums:
-            if current_sum < 0:
-                current_sum = 0
-            current_sum += num
-            max_sum = max(current_sum, max_sum)
+            # reset running_sum if the current running sum of array is below 0
+            if running_sum < 0:
+                running_sum = 0
+            # add num to running_sum
+            running_sum += num
+            # update max_sum if needed
+            max_sum = max(running_sum, max_sum)
+        # end of loop, return the max_sum
         return max_sum
         
