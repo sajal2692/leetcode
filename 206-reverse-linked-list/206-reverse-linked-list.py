b5c1ecf7dc20292f1e47b3f80fdc46323fce54fb
Sajal Sharma <sajal2692@gmail.com>
2022-09-15 14:26:54 +0530

    Time: 92 ms (5.44%), Space: 15.5 MB (28.30%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -5,13 +5,24 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        # need to keep track of previous, current and next node
         prev = None
         current = head
         
+        # iterate through the list
         while current:
+            # save the next node to a variable
+            # this is important so that after changing the link of the current node to previous,
+            # we still keep track of the next node in the list
             nxt = current.next
+            # point the current node to the previous element (on first iteration this will be None)
             current.next = prev
+            # move previous node pointer forward
             prev = current
+            # move the current node pointer forward (pointing it to the previously saved next node variable)
             current = nxt
+        # finally, point the head of the list to the previous node
+        # note: this will be the last node in the list after the while loop ends
         head = prev
         return head
