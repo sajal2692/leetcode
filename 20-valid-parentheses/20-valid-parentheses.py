b9cbd99a7c7649654287d98c993884dc6818889f
Sajal Sharma <sajal2692@gmail.com>
2022-10-01 18:31:39 +0530

    Time: 59 ms (34.95%), Space: 14 MB (26.51%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -6,17 +6,23 @@ class Solution:
             "]": "["
         }
         
+        # stack to track open parantheses
         open_char_stack = []
         
         for c in s:
+            # if c is a closing parantheses
             if c in close_2_open:
+                # if there is no open parantheses in the stack, return Fa;se
                 if len(open_char_stack) == 0:
                     return False
+                # pop the char at the top of the stack
                 open_char = open_char_stack.pop()
+                # the closing paran does not match opening paren, return False
                 if open_char != close_2_open[c]:
                     return False
             # it is an open paran character
             else:
+                # add to the top of the stack
                 open_char_stack.append(c)
         if len(open_char_stack) > 0:
             return False
