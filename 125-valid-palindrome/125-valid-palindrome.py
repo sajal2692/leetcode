02a81c0ba926814f8f0decafe6939e4d7a95c65a
Sajal Sharma <sajal2692@gmail.com>
2022-10-01 16:23:54 +0530

    Time: 182 ms (5.13%), Space: 14.4 MB (56.42%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,5 +1,21 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        filtered_s = "".join([c.lower() for c in s if c.isalnum()])
-        reversed_s = filtered_s[::-1]
-        return filtered_s == reversed_s
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            if not self.is_alphaNum(s[l]):
+                l += 1
+            elif not self.is_alphaNum(s[r]):
+                r -= 1
+            elif s[l].lower() == s[r].lower():
+                l += 1
+                r -= 1
+            else:
+                return False
+        return True
+    
+    def is_alphaNum(self, c):
+        "Returns if a given character is alphanumeric or not"
+        return ((ord('A') <= ord(c) <= ord('Z')) or
+                (ord('a') <= ord(c) <= ord('z')) or
+                (ord('0') <= ord(c) <= ord('9')))
