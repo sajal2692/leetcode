c0fd9e8d8a316a5855835d3b25f1deba39e3dce5
Sajal Sharma <sajal2692@gmail.com>
2022-10-01 13:54:43 +0530

    Time: 106 ms (28.76%), Space: 14.5 MB (34.87%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,5 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False
+        
         s_char_2_count = defaultdict(int)
         for c in s:
             s_char_2_count[c] += 1
@@ -10,10 +14,6 @@ class Solution:
         
         for c in s_char_2_count:
             if c not in t_char_2_count or s_char_2_count[c] != t_char_2_count[c]:
-                return False
-            
-        for c in t_char_2_count:
-            if c not in s_char_2_count or s_char_2_count[c] != t_char_2_count[c]:
                 return False
         
         return True
