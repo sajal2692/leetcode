a223bc612b7101f548e38801800589794b12d874
Sajal Sharma <sajal2692@gmail.com>
2022-10-01 14:04:23 +0530

    Time: 79 ms (59.17%), Space: 14.4 MB (67.07%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -4,16 +4,14 @@ class Solution:
         if len(s) != len(t):
             return False
         
-        s_char_2_count = defaultdict(int)
-        for c in s:
-            s_char_2_count[c] += 1
-        
-        t_char_2_count = defaultdict(int)
-        for c in t:
-            t_char_2_count[c] += 1
+        s_char_2_count = {}
+        t_char_2_count = {}
+        for i in range(len(s)):
+            s_char_2_count[s[i]] = 1 + s_char_2_count.get(s[i], 0) 
+            t_char_2_count[t[i]] = 1 + t_char_2_count.get(t[i], 0) 
         
         for c in s_char_2_count:
-            if c not in t_char_2_count or s_char_2_count[c] != t_char_2_count[c]:
+            if s_char_2_count[c] != t_char_2_count.get(c, 0):
                 return False
         
         return True
