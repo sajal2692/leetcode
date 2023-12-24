69dbe8e8fb5bc6a7317e2d01c8d7a73c00102fe6
Sajal Sharma <sajal2692@gmail.com>
2023-12-24 12:44:32 +0800

    Time: 189 ms (76.77%), Space: 25.8 MB (5.27%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -4,20 +4,29 @@ class Solution:
         prefix = [1] * len_nums
         postfix = [1] * len_nums
         output = []
+        
         # calculate prefixes
         for i in range(len_nums):
             if i == 0:
-                continue
-            prefix[i] = prefix[i-1] * nums[i-1]
+                prefix[i] = nums[i]
+            else:
+                prefix[i] = prefix[i-1] * nums[i]
         
         # calculate postfixes
         for i in range(len_nums - 1, -1, -1):
-            print(i)
             if i == len_nums - 1:
-                continue
-            postfix[i] = postfix[i+1] * nums[i+1]
+                postfix[i] = nums[i]
+            else:
+                postfix[i] = postfix[i+1] * nums[i]
 
+        # calculate output
         for i in range(len_nums):
-          output.append(prefix[i] * postfix[i])
+            if i == 0:
+                out = 1 * postfix[i + 1]
+            elif i == len_nums -1:
+                out = prefix[i - 1] * 1
+            else:
+                out = prefix[i-1] * postfix[i+1]
+            output.append(out)
 
         return output
