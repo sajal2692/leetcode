0feddbe219579d77eb104ffa7d86ed6639905ef7
Sajal Sharma <sajal2692@gmail.com>
2023-12-25 16:34:06 +0800

    Time: 56 ms (31.07%), Space: 17.8 MB (32.45%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -17,6 +17,3 @@ class Solution:
     
     def isAlphaNum(self, c):
         return (ord("a") <= ord(c) <= ord("z")) or (ord("A") <= ord(c) <= ord("Z")) or (ord("0") <= ord(c) <= ord("9"))
-            
-        
-#         ".;"
