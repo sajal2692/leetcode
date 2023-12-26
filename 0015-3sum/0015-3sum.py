552d91240d43cd298bd0fac579ae702c274c9030
Sajal Sharma <sajal2692@gmail.com>
2023-12-26 10:33:21 +0800

    Time: 642 ms (96.05%), Space: 21.5 MB (11.57%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,50 +1,23 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # result = []
-        # nums.sort()
-        # print(nums)
-        # len_nums = len(nums)
-        # for i, num in enumerate(nums):
-        #     if i > 0 and num == nums[i-1]:
-        #         continue
-        #     l, r = i + 1, len_nums - 1
-        #     while l < r:
-        #         if (l > i + 1 and nums[l] == nums[l-1]) and (r < len_nums - 1and nums[r] == nums[r+1]):
-        #             l += 1
-        #             r -= 1
-        #         elif nums[l] + nums[r] + num == 0:
-        #             result.append([num, nums[l], nums[r]])
-        #             l += 1
-        #             r -= 1
-        #         elif nums[l] + nums[r] + num < 0:
-        #             l += 1
-        #         elif nums[l] + nums[r] + num > 0:
-        #             r -= 1
-        # return result
-        
-        res = []
+        result = []
         nums.sort()
-
-        for i, a in enumerate(nums):
-            # Skip positive integers
-            # if a > 0:
-            #     break
-
-            if i > 0 and a == nums[i - 1]:
+        len_nums = len(nums)
+        for i, n in enumerate(nums):
+            if i > 0 and n == nums[i-1]:
                 continue
-
-            l, r = i + 1, len(nums) - 1
+            l, r = i + 1, len_nums - 1
             while l < r:
-                threeSum = a + nums[l] + nums[r]
-                if threeSum > 0:
-                    r -= 1
-                elif threeSum < 0:
+                three_sum = n + nums[l] + nums[r]
+                if three_sum < 0:
                     l += 1
+                elif three_sum > 0:
+                    r -= 1
                 else:
-                    res.append([a, nums[l], nums[r]])
+                    result.append([n, nums[l], nums[r]])
                     l += 1
                     r -= 1
-                    while nums[l] == nums[l - 1] and l < r:
+                    
+                    while l < r and nums[l] == nums[l-1]:
                         l += 1
-                        
-        return res
+        return result
