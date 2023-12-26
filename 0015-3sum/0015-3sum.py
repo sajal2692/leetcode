873cfdbb01a58971b10b6be3955cce946411576e
Sajal Sharma <sajal2692@gmail.com>
2023-12-26 10:34:19 +0800

    Time: 627 ms (96.72%), Space: 21.6 MB (11.57%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,11 +1,14 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         result = []
+        # sort so that can use two pointer approach
         nums.sort()
         len_nums = len(nums)
         for i, n in enumerate(nums):
+            # if the starting number is the same as the previous one in the list, continue
             if i > 0 and n == nums[i-1]:
                 continue
+            # set up two pointers
             l, r = i + 1, len_nums - 1
             while l < r:
                 three_sum = n + nums[l] + nums[r]
@@ -17,7 +20,7 @@ class Solution:
                     result.append([n, nums[l], nums[r]])
                     l += 1
                     r -= 1
-                    
+                    # if the left pointer is the same as the previous left pointer, move it ahead
                     while l < r and nums[l] == nums[l-1]:
                         l += 1
         return result
