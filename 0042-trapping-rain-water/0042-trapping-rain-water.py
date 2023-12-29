3ded26168abd9f969eaaea5bab2c9336748befe5
Sajal Sharma <sajal2692@gmail.com>
2023-12-29 14:58:09 +0800

    Time: 173 ms (8.56%), Space: 19.4 MB (7.35%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,28 +1,18 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # multi-pass solution
-        n = len(height)
-        maxLs = [0] * n
-        maxRs = [0] * n
+        # two pointer solution
+        # [0,1,0,2,1,0,1,3,2,1,2,1]
+        l, r = 0, len(height) - 1
+        maxL, maxR = height[l], height[r]
         amount = 0
-        
-        
-        # forward pass to calculate maxLs
-        for i in range(1, n, 1):
-            maxLs[i] = max(height[i-1], maxLs[i-1])
-        
-        # backward pass to calculate maxRs
-        for i in range(n-1, -1, -1):
-            if i == n - 1:
-                continue
-            maxRs[i] = max(height[i+1], maxRs[i+1])
-        
-        print(maxLs)
-        print(maxRs)
-        for i, h in enumerate(height):
-            a = min(maxLs[i], maxRs[i]) - h
-            amount += max(0, a)
-            
-        return amount
-        
+        while l < r:
+            if maxL < maxR:
+                l += 1
+                maxL = max(maxL, height[l])
+                amount += max(0, maxL -  height[l])
+            else:
+                r -= 1
+                maxR = max(maxR, height[r])
+                amount += max(0, maxR -  height[r])
+        return amount  
         
