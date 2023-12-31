d270844cd9ecdf1aed8e9da463d3fc020e918bee
Sajal Sharma <sajal2692@gmail.com>
2023-12-31 14:39:10 +0800

    Time: 870 ms (98.97%), Space: 32.4 MB (26.49%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,15 +1,10 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         answer = [0] * len(temperatures)
-        stack = [] # (i, t)
+        stack = []
         for i, t in enumerate(temperatures):
-            # while the last temperature in the stack is smaller than the current temperature
-            while stack and stack[-1][1] <  t:
-                # pop the stack
-                j, t2 = stack.pop()
-                # place the difference between the indices into the answer
-                answer[j] = i-j
-            stack.append((i, t))
+            while stack and temperatures[stack[-1]] < t:
+                stackI = stack.pop()
+                answer[stackI] = i - stackI
+            stack.append(i)
         return answer
-                    
-            
