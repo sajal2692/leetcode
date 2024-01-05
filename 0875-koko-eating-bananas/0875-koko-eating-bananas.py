1af596bdd3b5a4cb1ca23d7428804c8e68391889
Sajal Sharma <sajal2692@gmail.com>
2024-01-05 12:26:47 +0800

    Time: 286 ms (88.10%), Space: 18.8 MB (17.17%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -6,38 +6,23 @@ class Solution:
         
         # piles = [3, 6, 7, 11], len = 4,  h = 8, if eating speed is 4
         # steps_needed = piles // 4 (eating_speed) + 1 = [1, 2, 2, 3] ; total = 8
-        # 
-        
-        # [0, 6, 7, 11]
-        # [0, 2, 7, 11]
-        # [0, 0, 7, 11]
-        # [0, 0, 3, 11]
-        # [0, 0, 0, 11]
-        # [0, 0, 0, 7]
-        # [0, 0, 0, 3]
-        # [0, 0, 0, 0]
-        
-        # [30, 11, 23, 4, 20]
-        # [0, 11, 23, 4, 20]
-        # [0, 0, 23, 4, 20]
-        # [0, 0, 0, 4, 20]
-        # [0, 0, 0, 0, 0]
-        
-        
+
         # optimum solution
         l, r = 1, max(piles)
-        res = 0
+        res = 0 # need this to keep track of edge case where the last iteration of the binary search may lead to a ->
+        # <- mid point with more time taken than needed
         while l <= r:
             mid = (l + r) // 2
 
             total_time_taken = 0
             for p in piles:
                 time_taken = math.ceil(p / mid)
-                total_time_taken += time_taken # [2, 1, 1, 1, 1]
+                total_time_taken += time_taken 
             if total_time_taken > h:
                 l = mid + 1
             elif total_time_taken <= h:
                 r = mid - 1
+                # only assign the mid point to the result if the total time taken is <= the max limit (h)
                 res = mid
         return res
             
