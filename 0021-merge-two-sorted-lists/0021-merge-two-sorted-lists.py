3fe951051123671c073abe628afa7fbde8df3de1
Sajal Sharma <sajal2692@gmail.com>
2025-09-06 12:54:31 +0800

    Time: 0 ms (100%), Space: 12.6 MB (6.77%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,23 +1,29 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode(-1)
-        tail = dummy
-        
-        while list1 and list2:
-            if list1.val < list2.val:
-                dummy.next = list1
-                list1 = list1.next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode()
+        curr = dummy
+        curr_1 = list1
+        curr_2 = list2
+        while curr_1 and curr_2:
+            if curr_1.val < curr_2.val:
+                curr.next = curr_1
+                curr_1 = curr_1.next
             else:
-                dummy.next = list2
-                list2 = list2.next
-            dummy = dummy.next
-        
-        dummy.next = list1 or list2
-        
-        
-        return tail.next
+                curr.next = curr_2
+                curr_2 = curr_2.next
+            curr = curr.next
+        if curr_1:
+            curr.next = curr_1
+        if curr_2:
+            curr.next = curr_2
+        return dummy.next
