20a917f3418e2ff81a78bedc53ffb76b32af1692
Sajal Sharma <sajal2692@gmail.com>
2026-02-11 16:31:06 +0800

    Time: 0 ms (100%), Space: 12.8 MB (34.02%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,32 +1,34 @@
-class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        
-        top, bottom = 0, len(matrix) - 1
-        
-        # find the right row using binary search
-        while top <= bottom:
-            mid = (top + bottom) // 2
-            if target > matrix[mid][-1]:
-                top = mid + 1
-            elif target < matrix[mid][0]:
-                bottom = mid - 1
-            else:
-                break
-        
-        # pointers crossed, no element found
-        if not top <= bottom:
-            return False
-        
-        row = matrix[mid]
-        l, r = 0, len(row) - 1
+class Solution(object):
+    
+    def searchRow(self, row, target):
+        l = 0
+        r = len(row) - 1
         while l <= r:
             mid = (l + r) // 2
-            if target > row[mid]:
-                l = mid + 1
-            elif target < row[mid]:
-                r = mid - 1
-            else:
+            if row[mid] == target:
                 return True
+            elif row[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
         return False
-            
-            
+    
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        top = 0
+        bottom = len(matrix) - 1
+        left = 0
+        right = len(matrix[0]) -1
+        while top <= bottom:
+            mid = (top + bottom) // 2
+            if matrix[mid][left] > target:
+                bottom = mid - 1
+            elif matrix[mid][right] < target:
+                top = mid + 1
+            else:
+                return self.searchRow(matrix[mid], target)
+        return False
