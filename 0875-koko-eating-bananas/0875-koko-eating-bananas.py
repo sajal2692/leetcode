3650dc2101b7004d84a4f0d0a3c32b6c19533876
Sajal Sharma <sajal2692@gmail.com>
2026-02-13 13:02:41 +0800

    Time: 164 ms (62.29%), Space: 20.9 MB (9.16%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,31 +1,15 @@
-import math
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        # h is the number of steps (hour)
-        # k is the eating speed 
-        
-        # piles = [3, 6, 7, 11], len = 4,  h = 8, if eating speed is 4
-        # steps_needed = piles // 4 (eating_speed) + 1 = [1, 2, 2, 3] ; total = 8
-
-        # optimum solution
-        l, r = 1, max(piles)
-        res = 0 # need this to keep track of edge case where the last iteration of the binary search may lead to a ->
-        # <- mid point with more time taken than needed
+        l = 1
+        r = max(piles)
         while l <= r:
-            mid = (l + r) // 2
-
-            total_time_taken = 0
-            for p in piles:
-                time_taken = math.ceil(p / mid)
-                total_time_taken += time_taken 
-            if total_time_taken > h:
-                l = mid + 1
-            elif total_time_taken <= h:
-                r = mid - 1
-                # only assign the mid point to the result if the total time taken is <= the max limit (h)
-                res = mid
-        return res
-            
+            speed = (l+r)//2
+            hours = 0
+            for pile in piles:
+                hours += math.ceil(pile / speed)
+            if hours > h:
+                l = speed + 1
+            else:
+                r = speed - 1
+        return l
         
-        # [1, 30] = (15) 8 > h 5
-        # [16, 30] = (23)
