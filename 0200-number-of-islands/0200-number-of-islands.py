b3335b20c1c7eac1f1d25b75239c0cd048dd2f5c
Sajal Sharma <sajal2692@gmail.com>
2026-03-02 20:20:00 +0800

    Time: 268 ms (22.15%), Space: 26.3 MB (27.59%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,23 +1,21 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        def findIslands(grid, r, c, visited, island, num_rows, num_cols):
+        def findIslands(grid, r, c, visited, num_rows, num_cols):
             # check out of bounds
             if r < 0 or c < 0 or r == num_rows or c == num_cols or (r,c) in visited:
-                return 0
+                return False
             if grid[r][c] == '0':
-                return 0
+                return False
             
             visited.add((r,c))
-            island.add((r,c))
 
-            findIslands(grid, r-1, c, visited, island, num_rows, num_cols)
-            findIslands(grid, r+1, c, visited, island, num_rows, num_cols)
-            findIslands(grid, r, c-1, visited, island, num_rows, num_cols)
-            findIslands(grid, r, c+1, visited, island, num_rows, num_cols)
+            findIslands(grid, r-1, c, visited, num_rows, num_cols)
+            findIslands(grid, r+1, c, visited, num_rows, num_cols)
+            findIslands(grid, r, c-1, visited, num_rows, num_cols)
+            findIslands(grid, r, c+1, visited, num_rows, num_cols)
 
-            if island:
-                return 1
+            return True
 
         island_count = 0
         visited = set()
@@ -25,7 +23,8 @@ class Solution:
 
         for i in range(num_rows):
             for j in range(num_cols):
-                island_count += findIslands(grid, i, j, visited, set(), num_rows, num_cols)
+                if findIslands(grid, i, j, visited, num_rows, num_cols):
+                    island_count += 1
         
         return island_count
 
