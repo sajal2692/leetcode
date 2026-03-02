6d144b52b5e38b3f201e8ae927644855d289bd60
Sajal Sharma <sajal2692@gmail.com>
2026-03-02 20:21:41 +0800

    Time: 259 ms (32.24%), Space: 31.1 MB (11.88%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,31 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        
-        def findIslands(grid, r, c, visited, num_rows, num_cols):
-            # check out of bounds
+        visited = set()
+        num_rows, num_cols = len(grid), len(grid[0])
+
+        def dfs(r, c):
             if r < 0 or c < 0 or r == num_rows or c == num_cols or (r,c) in visited:
                 return False
             if grid[r][c] == '0':
                 return False
-            
-            visited.add((r,c))
 
-            findIslands(grid, r-1, c, visited, num_rows, num_cols)
-            findIslands(grid, r+1, c, visited, num_rows, num_cols)
-            findIslands(grid, r, c-1, visited, num_rows, num_cols)
-            findIslands(grid, r, c+1, visited, num_rows, num_cols)
-
+            visited.add((r, c))
+            dfs(r-1, c)
+            dfs(r+1, c)
+            dfs(r, c-1)
+            dfs(r, c+1)
             return True
 
         island_count = 0
-        visited = set()
-        num_rows, num_cols = len(grid), len(grid[0])
-
         for i in range(num_rows):
             for j in range(num_cols):
-                if findIslands(grid, i, j, visited, num_rows, num_cols):
+                if dfs(i, j):
                     island_count += 1
-        
+
         return island_count
-
-
