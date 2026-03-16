1dc32da4c60a4266c8c7a1fa6e3626a6e5a09417
Sajal Sharma <sajal2692@gmail.com>
2026-03-16 13:57:34 +0800

    Time: 0 ms (100%), Space: 19.3 MB (79.58%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,15 +1,31 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-
-        def climb(n, cache):
-            if n in cache:
-                return cache[n]
-            if n == 0:
-                return 1
-            if n < 0:
-                return 0
+        
+        # top down dp / memoization
+        # def climb(n, cache):
+        #     if n in cache:
+        #         return cache[n]
+        #     if n == 0:
+        #         return 1
+        #     if n < 0:
+        #         return 0
             
-            cache[n] = climb(n-1, cache) + climb(n-2, cache)
-            return cache[n]
+        #     cache[n] = climb(n-1, cache) + climb(n-2, cache)
+        #     return cache[n]
     
-        return climb(n, {})
+        # return climb(n, {})
+
+        if n == 0:
+            return 1
+        if n < 0:
+            return 0
+        
+        dp = [1, 1] # init with ways to climb for 0 and 1
+
+        i = 2
+        while i <= n:
+            tmp = dp[1]
+            dp[1] = dp[1] + dp[0]
+            dp[0] = tmp
+            i += 1
+        return dp[1]
