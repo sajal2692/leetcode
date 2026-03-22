ce7d2bf817e0872254a4e6b134f674b0706a07fc
Sajal Sharma <sajal2692@gmail.com>
2026-03-22 13:37:49 +0800

    Time: 0 ms (100%), Space: 19.2 MB (97.64%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,17 +1,30 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         
-        cache = {}
+        # memoization approach (top down dp)
+        # cache = {}
 
-        def memo(i, j):
-            if i >= m or j >= n:
-                return 0
-            if (i,j) in cache:
-                return cache[(i,j)]
-            if i == m - 1 and j == n - 1:
-                return 1
+        # def memo(i, j):
+        #     if i >= m or j >= n:
+        #         return 0
+        #     if (i,j) in cache:
+        #         return cache[(i,j)]
+        #     if i == m - 1 and j == n - 1:
+        #         return 1
             
-            cache[(i,j)] = memo(i+1,j) + memo(i, j+1)
-            return cache[(i,j)]
+        #     cache[(i,j)] = memo(i+1,j) + memo(i, j+1)
+        #     return cache[(i,j)]
         
-        return memo(0,0)
+        # return memo(0,0)
+
+        # bottom up dp
+
+        prev_row = [0] * n
+
+        for i in range(m - 1, -1, -1):
+            cur_row = [0] * n
+            cur_row[n-1] = 1
+            for j in range(n - 2, -1, -1):
+                cur_row[j] = cur_row[j+1] + prev_row[j]
+            prev_row = cur_row
+        return prev_row[0]
